8a76f097e1191cc1fed308ef1d5f9067569021fa
Ulrich Riemenschneider <mail@ulrichriemenschneider.de>
2025-12-18 21:42:32 +0100

    Constant STAGES moved to a separate file and imported into snowman.py

--- snowman.py
+++ snowman.py
@@ -1,34 +1,5 @@
 import random
-
-# Snowman ASCII Art stages
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
+from ascii_art import STAGES
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
